4b70aa73a56e7ead816c75fd4a5d63d8d9f8469e
Calvin Roberts <roberts.calvin85@gmail.com>
2021-08-05 20:23:12 +0100

    Add animation delay to blocks class

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const banner = document.getElementsByClassName('banner')[0],
 
 for (let i = 1; i < 400; i++) {
   banner.innerHTML += "<div class='blocks'></div>";
+  blocks[i].style.animationDelay = `${i * 0.05}s`;
 }
